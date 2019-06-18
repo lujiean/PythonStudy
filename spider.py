73541077e26f01a7f9304b5ee84ad5c61540d44b
Lu <jiean.a.lu@accenture.com>
2019-06-18 11:29:28 +0800

    18062019

--- spider.py
+++ spider.py
@@ -72,69 +72,40 @@ def TestSpider2(rootPath):
     logging.info(time.asctime())
 
     # proxy={'http':'http://127.0.0.1:8888', 'https':'https://127.0.0.1:8888'}
-    # proxy={}
-    proxy=dupanConfig["proxy"]
 
-    # --s1 
-    # url="https://pan.baidu.com/s/1pUZRD5wJOM7iUA4_O-TRSw"
-    # headers = {
-    #     "Host": "pan.baidu.com",
-    #     "Connection": "keep-alive",
-    #     "Upgrade-Insecure-Requests": "1",
-    #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
-    #     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-    #     "Accept-Encoding": "gzip, deflate, br",
-    #     "Accept-Language": "zh-CN,zh;q=0.9"
-    # }
+    # --s1 first request
     curStep = "step1"
     if curStep in dupanConfig:
         dupanConfig[curStep]["url"] = url
-        # headers = dupanConfig["step1"]["headers"]
-        # allow_redirects = dupanConfig["step1"]["allow_redirects"]
-        # resp = requests.get(url, proxies=proxy, headers=headers, allow_redirects=False)
         resp = requests.get(url=dupanConfig[curStep]["url"], 
                             proxies=dupanConfig["proxy"], 
                             headers=dupanConfig[curStep]["headers"], 
                             allow_redirects=dupanConfig[curStep]["allow_redirects"])
         resp.encoding = 'UTF-8'
-    
-        # PrintHttpDetails(resp)
 
         # set redirect url
         if resp.status_code == 302 and 'Location' in resp.headers:
             url3=resp.headers['Location']
-        # print(url)    
+        cookieDict = spiderutils.GetCookieDict(resp)
+        cookie = spiderutils.GetCookieStrFromDict(cookieDict)
         logging.info(resp.status_code)
     else:
         logging.info("No "+curStep)
     # --
 
     # --s2
-    cookieDict = spiderutils.GetCookieDict(resp)
-    cookie = spiderutils.GetCookieStrFromDict(cookieDict)
-    # print(str)
-    # headers = {
-    #     "Host": "pan.baidu.com",
-    #     "Connection": "keep-alive",
-    #     "Upgrade-Insecure-Requests": "1",
-    #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
-    #     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-    #     "Accept-Encoding": "gzip, deflate, br",
-    #     "Accept-Language": "zh-CN,zh;q=0.9",
-    #     "Cookie": cookie
-    # }
     curStep="step2"
     if curStep in dupanConfig:
 
         dupanConfig[curStep]["url"]=url3
         dupanConfig[curStep]["headers"]["Cookie"]=cookie
-        # resp = requests.get(url3, proxies=proxy, headers=headers, allow_redirects=False)
+        
         resp = requests.get(url=dupanConfig[curStep]["url"], 
                             proxies=dupanConfig["proxy"], 
                             headers=dupanConfig[curStep]["headers"], 
                             allow_redirects=dupanConfig[curStep]["allow_redirects"])
         resp.encoding = 'UTF-8'
-    # PrintHttpDetails(resp)
+
         logging.info(resp.status_code)
     else:
         logging.info("No "+curStep)
@@ -143,7 +114,7 @@ def TestSpider2(rootPath):
     # --s3: post pick_up_code request
     curStep="step3"
     if curStep in dupanConfig:
-        # url2 = "https://pan.baidu.com/share/verify"
+        
         surl = parse.parse_qs(parse.urlparse(url3).query)['surl'][0]
         ts13 = str(int(time.time()*1000))
         ts10 = str(int(time.time()))
@@ -153,66 +124,28 @@ def TestSpider2(rootPath):
         ctx = execjs.compile(content)
         logid = ctx.call("Getlogid", cookieDict["BAIDUID"])
 
+        #params
         dupanConfig[curStep]["params"]["surl"]=surl
         dupanConfig[curStep]["params"]["t"]=ts13
         dupanConfig[curStep]["params"]["logid"]=logid
-    # params = {
-    #     # "surl": "pUZRD5wJOM7iUA4_O-TRSw",
-    #     "surl": surl,
-    #     # "t": "1559704493678",
-    #     "t": ts13,
-    #     "channel": "chunlei",
-    #     "web": "1",
-    #     "app_id": "250528",
-    #     "bdstoken": "null",
-    #     # "logid": "MTU1OTcwNDQ5MzY4MTAuNTM0OTc1MDYyNzc4NTI2OA==",
-    #     "logid": logid,
-    #     "clienttype": "0"
-    # }
-    # print(params)
 
-    # Hm_lvt_7a3960b6f067eb0085b7f96ff5e660b0=1559660491; Hm_lpvt_7a3960b6f067eb0085b7f96ff5e660b0=1559660491"
+        #headers
         cookieDict["Hm_lvt_7a3960b6f067eb0085b7f96ff5e660b0"]=ts10
         cookieDict["Hm_lpvt_7a3960b6f067eb0085b7f96ff5e660b0"]=ts10
         cookie = spiderutils.GetCookieStrFromDict(cookieDict)
         dupanConfig[curStep]["headers"]["Referer"]=url3
         dupanConfig[curStep]["headers"]["Cookie"]=cookie
-    # headers = {
-    #     "Host": "pan.baidu.com",
-    #     "Connection": "keep-alive",
-    #     "Content-Length": "26",
-    #     "Accept": "*/*",
-    #     "Origin": "https://pan.baidu.com",
-    #     "X-Requested-With": "XMLHttpRequest",
-    #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
-    #     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-    #     # "Referer": "https://pan.baidu.com/share/init?surl=pUZRD5wJOM7iUA4_O-TRSw",
-    #     "Referer": url3,
-    #     "Accept-Encoding": "gzip, deflate, br",
-    #     "Accept-Language": "zh-CN,zh;q=0.9",
-    #     # "Cookie": "PANWEB=1; BAIDUID=D85812618FC241EA800013BE1D970555:FG=1; Hm_lvt_7a3960b6f067eb0085b7f96ff5e660b0=1559660491; Hm_lpvt_7a3960b6f067eb0085b7f96ff5e660b0=1559660491"
-    #     "Cookie": cookie
-    # }
-    # print(headers)
 
-    # pick_up_code = "fptt"
+        #data
         dupanConfig[curStep]["data"]["pwd"]=pick_up_code
-    # data = {
-    #     # pwd=fptt&vcode=&vcode_str=
-    #     "pwd": pick_up_code,
-    #     "vcode": "",
-    #     "vcode_str": ""
-    # }
-    # print(data)
 
-    # resp = requests.post(url2, proxies=proxy, data=data, params=params, headers=headers)
         resp = requests.post(url=dupanConfig[curStep]["url"], 
                              proxies=dupanConfig["proxy"], 
                              params=dupanConfig[curStep]["params"], 
                              headers=dupanConfig[curStep]["headers"],
                              data=dupanConfig[curStep]["data"])
         resp.encoding = 'UTF-8'
-        # PrintHttpDetails(resp)
+        
         randsk=json.loads(resp.text, encoding='UTF-8').get('randsk')
         logging.info(resp.status_code)
     else:
@@ -220,93 +153,46 @@ def TestSpider2(rootPath):
     # --
 
     # s4--
+    cookieDict["BDCLND"] = randsk
+    cookie = spiderutils.GetCookieStrFromDict(cookieDict)
+
     curStep="step4"
     if curStep in dupanConfig:
-        cookieDict["BDCLND"] = randsk
-        cookie = spiderutils.GetCookieStrFromDict(cookieDict)
+        # cookieDict["BDCLND"] = randsk
+        # cookie = spiderutils.GetCookieStrFromDict(cookieDict)
         dupanConfig[curStep]["url"]=url
-        dupanConfig[curStep]["Referer"]=url3
-        dupanConfig[curStep]["Cookie"]=cookie
-        # headers = {
-        #     "Host": "pan.baidu.com",
-        #     "Connection": "keep-alive",
-        #     "Upgrade-Insecure-Requests": "1",
-        #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
-        #     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-        #     # "Referer": "https://pan.baidu.com/share/init?surl=pUZRD5wJOM7iUA4_O-TRSw",
-        #     "Referer": url3,
-        #     "Accept-Encoding": "gzip, deflate, br",
-        #     "Accept-Language": "zh-CN,zh;q=0.9",
-        #     # "Cookie": "PANWEB=1; BAIDUID=D85812618FC241EA800013BE1D970555:FG=1; Hm_lvt_7a3960b6f067eb0085b7f96ff5e660b0=1559660491; Hm_lpvt_7a3960b6f067eb0085b7f96ff5e660b0=1559660491; BDCLND=hSsksGDHeTYSAu2txg7Kf2tiyhJe9jPOm5MgJ0N%2FwiI%3D"
-        #     "Cookie": cookie
-        # }
-
-        # resp = requests.get(url, proxies=proxy, headers=headers)
+        dupanConfig[curStep]["headers"]["Referer"]=url3
+        dupanConfig[curStep]["headers"]["Cookie"]=cookie
+        
         resp = requests.get(url=dupanConfig[curStep]["url"], 
                             proxies=dupanConfig["proxy"], 
                             headers=dupanConfig[curStep]["headers"])
         resp.encoding = 'UTF-8'
+        logging.info(resp.status_code)
     else:
         logging.info("No "+curStep)
-    # soup = BeautifulSoup(resp.text, "html.parser")
-    # print(soup.prettify())
-    # PrintHttpDetails(resp)
     # --
 
-    # s5--
+    # s5--list all files.
     curStep="step5"
     if curStep in dupanConfig:
+        #--params
         logid2 = ctx.call("Getlogid", cookieDict["BAIDUID"])
         dupanConfig[curStep]["params"]["logid"]=logid2
-        # params = {
-        #     "uk":"1862740414",
-        #     "shareid":"136149483",
-        #     "order":"other",
-        #     "desc":"1",
-        #     "showempty":"0",
-        #     "web":"1",
-        #     "page":"1",
-        #     "num":"100",
-        #     "dir":"/幪面超人Build",
-        #     "t":"0.5237814128347804",
-        #     "channel":"chunlei",
-        #     "app_id":"250528",
-        #     "bdstoken":"null",
-        #     "logid":logid2,
-        #     "clienttype": "0"
-        # }
 
+        #--headers
         cookieDict["cflag"] = "13%3A3"
         ts10_2 = str(int(time.time()))
         cookieDict["Hm_lpvt_7a3960b6f067eb0085b7f96ff5e660b0"]=ts10_2
         cookie = spiderutils.GetCookieStrFromDict(cookieDict)
         dupanConfig[curStep]["headers"]["Referer"]=url
         dupanConfig[curStep]["headers"]["Cookie"]=cookie
-        # headers = {
-        #     "Host": "pan.baidu.com",
-        #     "Connection": "keep-alive",
-        #     "Accept": "application/json, text/javascript, */*; q=0.01",
-        #     "X-Requested-With": "XMLHttpRequest",
-        #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
-        #     # "Referer": "https://pan.baidu.com/s/1pUZRD5wJOM7iUA4_O-TRSw",
-        #     "Referer": url,
-        #     "Accept-Encoding": "gzip, deflate, br",
-        #     "Accept-Language": "zh-CN,zh;q=0.9",
-        #     # "Cookie": "PANWEB=1; BAIDUID=D85812618FC241EA800013BE1D970555:FG=1; Hm_lvt_7a3960b6f067eb0085b7f96ff5e660b0=1559660491; BDCLND=hSsksGDHeTYSAu2txg7Kf2tiyhJe9jPOm5MgJ0N%2FwiI%3D; Hm_lpvt_7a3960b6f067eb0085b7f96ff5e660b0=1560172897; cflag=13%3A3"
-        #     "Cookie": cookie
-        # }
 
-        # url4 = "https://pan.baidu.com/share/list"
         resp = requests.get(url=dupanConfig[curStep]["url"],
                             proxies=dupanConfig["proxy"],
                             params=dupanConfig[curStep]["params"],
                             headers=dupanConfig[curStep]["headers"])
         resp.encoding = 'UTF-8'
-
-        # soup = BeautifulSoup(resp.text, "html.parser")
-        # print(soup.prettify())
-        # spiderutils.PrintHttpDetails(resp)
-        # print(resp.status_code)
         logging.info(resp.status_code)
     else:
         logging.info("No "+curStep)
@@ -314,10 +200,6 @@ def TestSpider2(rootPath):
 
     #-- save JSON
     jsonDict = json.loads(resp.text,encoding="UTF-8")
-    # del jsonDict['request_id']
-    # del jsonDict['server_time']
-    # for i in jsonDict.get('list')[:]:
-    #     print(i["server_filename"])
     jPath = cwd + "/json"
     if not os.path.exists(jPath):
         os.makedirs(jPath)
@@ -329,7 +211,6 @@ def TestSpider2(rootPath):
         with open(jFileName, "r", encoding="UTF-8") as f:
             jloadDict = json.load(f)
         #compare different
-        # nfl = jsonDict["list"]
         difflist = []
         for i in jsonDict["list"]:
             if i not in jloadDict["list"]:
@@ -341,19 +222,11 @@ def TestSpider2(rootPath):
             logging.info("No change")
         else:
             # print("Been changed")
-            # logging.info("Been changed")
             logging.info("Been changed:{0}".format(difflist.__str__()))
-            # logging.info("")
             subject = ""
             for i in difflist:
                 subject = subject + i["path"] + ","
             #send notification email
-            # with open("./config/spider.cfg","r", encoding="UTF-8") as f:
-            #     cfgText = json.load(f)
-            #     username = cfgText["email"]["username"]
-            #     passwd = cfgText["email"]["passwd"]
-            #     receiver = cfgText["email"]["receiver"]
-
             # try:
             smtp = smtplib.SMTP_SSL("smtp.163.com", 465)
             smtp.login(username, passwd)
